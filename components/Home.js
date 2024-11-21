b15e6c50e662d20402a89061c6ad0f60db934cfe
Damien <damien.mourot.91@gmail.com>
2024-11-21 12:01:42 +0100

    Redirect after login

--- components/Home.js
+++ components/Home.js
@@ -1,12 +1,84 @@
 import { useState, useEffect } from 'react'
 import { login } from '../reducers/user'
 import { useDispatch, useSelector } from 'react-redux';
-import { redirect } from 'next/navigation'
-
+import { useRouter } from 'next/router'
 function Home() {
+  const router = useRouter()
   const [isSignUp, setIsSignup] = useState(true)
 
-
+  const SingupModal = ({ connectionType }) => {
+    console.log({connectionType});
+    
+    const dispatch = useDispatch()
+    const user = useSelector((state) => state.user.value)
+    const [firstname, setFirstname] = useState('')
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+  
+  
+    useEffect(() => {
+      console.log('debug useEffect', { user });
+  
+    })
+  
+    const fetchBackForSignupSignin = async (type) => {
+      console.log('debug', { type });
+  
+      if (type === 'signup') {
+        console.log('debug1');
+  
+        const response = await fetch('http://localhost:3000/users/signup', {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({ firstname, username, password })
+        })
+        const dataFromBack = await response.json()
+        if (dataFromBack.result) {
+          dispatch(login({ token: dataFromBack.user.token, username: dataFromBack.user.username }))
+          router.push('/feed')
+        }
+      }
+    }
+  
+    return (
+      <div className='modal'>
+        <img src='/icons/twitter.png' />
+        <div className='firstname'>
+          <input
+            type='text'
+            className='firstname-input'
+            placeholder='Firstname'
+            value={firstname}
+            onChange={(e) => setFirstname(e.target.value)}
+          ></input>
+        </div>
+        <div className='username'>
+          <input
+            type='text'
+            className='username-input'
+            placeholder='Username'
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          ></input>
+        </div>
+        <div className='password'>
+          <input
+            type='password'
+            className='password-input'
+            placeholder='Password'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          ></input>
+        </div>
+        <button
+          className='button button-signup'
+          onClick={() => fetchBackForSignupSignin(connectionType)}
+        >
+          Sign up
+        </button>
+      </div>
+    )
+  }
 
   return (
     <div className='home-container'>
@@ -36,78 +108,3 @@ function Home() {
 }
 
 export default Home;
-
-
-const SingupModal = ({ connectionType }) => {
-  console.log({connectionType});
-  
-  const dispatch = useDispatch()
-  const user = useSelector((state) => state.user.value)
-  const [firstname, setFirstname] = useState('')
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
-
-
-  useEffect(() => {
-    console.log('debug useEffect', { user });
-
-  })
-
-  const fetchBackForSignupSignin = async (type) => {
-    console.log('debug', { type });
-
-    if (type === 'signup') {
-      console.log('debug1');
-
-      const response = await fetch('http://localhost:3000/users/signup', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ firstname, username, password })
-      })
-      const dataFromBack = await response.json()
-      if (dataFromBack.result) {
-        dispatch(login({ token: dataFromBack.user.token, username: dataFromBack.user.username }))
-        redirect('/feed')
-      }
-    }
-  }
-
-  return (
-    <div className='modal'>
-      <img src='/icons/twitter.png' />
-      <div className='firstname'>
-        <input
-          type='text'
-          className='firstname-input'
-          placeholder='Firstname'
-          value={firstname}
-          onChange={(e) => setFirstname(e.target.value)}
-        ></input>
-      </div>
-      <div className='username'>
-        <input
-          type='text'
-          className='username-input'
-          placeholder='Username'
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        ></input>
-      </div>
-      <div className='password'>
-        <input
-          type='password'
-          className='password-input'
-          placeholder='Password'
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        ></input>
-      </div>
-      <button
-        className='button button-signup'
-        onClick={() => fetchBackForSignupSignin(connectionType)}
-      >
-        Sign up
-      </button>
-    </div>
-  )
-}
