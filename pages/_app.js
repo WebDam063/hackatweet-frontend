6fbf74a5043a0c103849895ba83c7809379fa49c
groovyhooked <dafrenchie2002@yahoo.fr>
2024-11-21 11:00:54 +0100

    Redux toolkit & redux persist

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,35 @@
 import '../styles/globals.css';
 import '../components/home.css';
-
 import Head from 'next/head';
+
+import { persistStore, persistReducer } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
+import storage from 'redux-persist/lib/storage';
+
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+
+const reducers = combineReducers({ user });
+
+const persistConfig = { key: 'hackatweet', storage };
+
+const store = configureStore({
+  reducer: persistReducer(persistConfig, reducers),
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({ serializableCheck: false }),
+});
+
+const persistor = persistStore(store);
+
 
 function App({ Component, pageProps }) {
   return (
-    <>
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
       <Head>
         <title>Next.js App</title>
       </Head>
       <Component {...pageProps} />
-    </>
+      </PersistGate>
+    </Provider>
   );
 }
 
