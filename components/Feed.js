7411bba4bbecf8d245ca6fd62599099496a0cf74
Damien <damien.mourot.91@gmail.com>
2024-11-21 16:46:35 +0100

    Added Feed Module

--- components/Feed.js
+++ components/Feed.js
@@ -5,16 +5,22 @@ import { useRouter } from 'next/router'
 import  styles  from '../styles/Feed.module.css';
 import LogoutSection from './LogoutSection';
 import TrendsSection from './TrendsSection'
-
+import FeedList from './FeedList'
 function Feed(){
   return (
     <div className={styles.body}>
+
       {/* // Logout section below */}
      <LogoutSection />
+
      {/* Feed section below */}
+      <div className={styles.feed_section}>
+        <FeedList />
+      </div>
 
      {/* Trends section below */}
      <TrendsSection/>
+     
     </div>
   )
 }
