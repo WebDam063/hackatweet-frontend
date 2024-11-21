a136d9a8ec682196e3e71b9576b6972cf96474e7
Damien <damien.mourot.91@gmail.com>
2024-11-21 15:25:54 +0100

    Added hashtags container

--- components/TrendsSection.js
+++ components/TrendsSection.js
@@ -1,16 +1,26 @@
 import styles from '../styles/Trends.module.css';
 import { useDispatch } from 'react-redux';
 
-
 function TrendsSection() {
     const dispatch = useDispatch();
+
+    
+
     return (
         <div className={styles.trends_section}>
                 <h1 className={styles.title}>Trends</h1>
             <div className={styles.trendContainer}>
                 <div className={styles.row}>
                     <h1>Test</h1>
-                    <span></span>
+                    <span>x items</span>
+                </div>
+                <div className={styles.row}>
+                    <h1>Test</h1>
+                    <span>x items</span>
+                </div>
+                <div className={styles.row}>
+                    <h1>Test</h1>
+                    <span>x items</span>
                 </div>
             </div>
         </div>
