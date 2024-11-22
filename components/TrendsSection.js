e3b59dfbfef82a232daf8f8c98744d3adbf911e9
groovyhooked <dafrenchie2002@yahoo.fr>
2024-11-22 10:48:31 +0100

    Trend section with hashtags displayed

--- components/TrendsSection.js
+++ components/TrendsSection.js
@@ -1,27 +1,42 @@
 import styles from '../styles/Trends.module.css';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
 
 function TrendsSection() {
+    const [hashtags, setHashtags] = useState([])
     const dispatch = useDispatch();
+    const user = useSelector((state) => state.user.value)
+    console.log(user);
 
-    
+    useEffect(() => {
+        const fetchTrend = async () => {
+            const response = await fetch('http://localhost:3000/tweets/trend', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ username: user.username })
+            })
+            const trendFromBack = await response.json()
+            console.log(trendFromBack.hashtags);
+            setHashtags(trendFromBack.hashtags)
+        }
+        fetchTrend()
+    }, [])
+
+
+    const hashtags_array = hashtags.map((hashtag, index) => {
+        return (
+            <div className={styles.row} key={index}>
+                <h1 className={styles.hashtag}>{hashtag._id}</h1>
+                <span>{hashtag.count} tweet{hashtag.count > 1 && 's'}</span>
+            </div >
+        )
+    })
 
     return (
         <div className={styles.trends_section}>
-                <h1 className={styles.title}>Trends</h1>
+            <h1 className={styles.title}>Trends</h1>
             <div className={styles.trendContainer}>
-                <div className={styles.row}>
-                    <h1>Test</h1>
-                    <span>x items</span>
-                </div>
-                <div className={styles.row}>
-                    <h1>Test</h1>
-                    <span>x items</span>
-                </div>
-                <div className={styles.row}>
-                    <h1>Test</h1>
-                    <span>x items</span>
-                </div>
+                {hashtags_array.length > 1 ? hashtags_array : <p className={styles.notrend}>No trends yet</p>}
             </div>
         </div>
     )
